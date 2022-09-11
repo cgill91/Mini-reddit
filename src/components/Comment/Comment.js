e152d827eca113349858fbeb0ad4b230564c9169
cgill91 <coltongillmore09@gmail.com>
2022-09-11 12:04:28 -0500

    comment components

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { timeConverter } from "../../utils/timeConverter";
+import "./Comment.css";
+import Markdown from "markdown-to-jsx";
+
+function Comment({ author, body, created }) {
+  let time = timeConverter(created);
+  return (
+    <div className="comment-container">
+      <p className="author">{author}</p>
+      <Markdown options={{ wrapper: "article" }}>{body}</Markdown>
+      <h6>{time}</h6>
+    </div>
+  );
+}
+
+export default Comment;
